1a91c2c494928ac352da5b426fa1c9d1dd2d177a
Nitin <nitinsokhal@gmail.com>
2024-11-22 20:36:12 +0530

    Java Selenium Q spiders

--- Selenium 3pm/Automation/src/openAndClosing/openingClosing.java
+++ Selenium 3pm/Automation/src/openAndClosing/openingClosing.java
@@ -9,18 +9,18 @@ import java.time.Duration;
 public class openingClosing {
 	public static void main(String[] args) throws Exception {
 		
-		// Initialize the WebDriver
-        WebDriver driver = new ChromeDriver();
-        
-        // Open a website (e.g., Google)
-        driver.get("https://www.linkedin.com");
-        
-        // Wait for a few seconds to see the browser open
-        
-            Thread.sleep(3000); // 3-second pause
-        
-        // Close the browser
-        driver.quit();
+			// Initialize the WebDriver
+	        WebDriver driver = new ChromeDriver();
+	        
+	        // Open a website (e.g., Google)
+	        driver.get("https://www.linkedin.com");
+	        
+	        // Wait for a few seconds to see the browser open
+	        
+	         Thread.sleep(3000); // 3-second pause
+	        
+	        // Close the browser
+	        driver.quit();
 		
 	
 
