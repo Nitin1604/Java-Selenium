2b8bcd73dc151d4be8f685486a692edd6d3301f1
Nitin <nitinsokhal@gmail.com>
2024-12-06 19:09:29 +0530

    Automating Online Mailing System

---
+++ Selenium 3pm/Automation/src/AutomateOnlineMailingSystem/LoginPage.java
@@ -0,0 +1,32 @@
+package AutomateOnlineMailingSystem;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginPage {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://localhost:8080/oms/");
+		driver.manage().window().maximize();
+		
+		// Selecting the email text fields
+		WebElement emailTextField = driver.findElement(By.tagName("input"));
+		Thread.sleep(1000);
+		emailTextField.sendKeys("nitinsokhal@gmail.com");
+		
+		// Selecting the password text fields
+		WebElement passwordTextField = driver.findElement(By.name("pass"));
+		Thread.sleep(2000);
+		passwordTextField.sendKeys("123");
+		
+		// Login button was clicked
+		WebElement loginBtn = driver.findElement(By.cssSelector("input.col-sm-push-3"));
+		Thread.sleep(3000);
+		loginBtn.click();
+		
+	}
+
+}
---
+++ Selenium 3pm/Automation/src/AutomateOnlineMailingSystem/NewUser.java
@@ -0,0 +1,48 @@
+package AutomateOnlineMailingSystem;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NewUser {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.navigate().to("http://localhost:8080/oms/NewUser.html");
+		driver.manage().window().maximize();
+		
+		// Filling details in the name text field
+     	WebElement nameTextField = driver.findElement(By.name("name"));
+		Thread.sleep(2000);
+		nameTextField.sendKeys("Rohan Singh");
+		
+		// Filling details in the email text field
+		WebElement emailTextField = driver.findElement(By.name("email"));
+		Thread.sleep(2000);
+		emailTextField.sendKeys("rohansingh@gmail.com");
+		
+		// Filling the details in the password text field
+		WebElement passwordTextField = driver.findElement(By.name("pass"));
+		Thread.sleep(2000);
+		passwordTextField.sendKeys("654321");
+		
+		// Filling the details in the phone input text field
+		WebElement phoneTextField = driver.findElement(By.name("phone"));
+		Thread.sleep(2000);
+		phoneTextField.sendKeys("123456789");
+		
+		// Clicking on "Create New Account" button to create an account
+		WebElement accountBtn = driver.findElement(By.className("col-sm-push-3"));
+		Thread.sleep(2000);
+		accountBtn.click();
+		
+		
+		
+		
+		
+
+	}
+
+}
