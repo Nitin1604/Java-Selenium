2d403054c8ad89e38fc16127b66be939c37632b0
Nitin <nitinsokhal@gmail.com>
2024-12-06 21:30:34 +0530

    Automating Online Mailing System

--- Selenium 3pm/Automation/src/AutomateOnlineMailingSystem/NewUser.java
+++ Selenium 3pm/Automation/src/AutomateOnlineMailingSystem/NewUser.java
@@ -14,7 +14,7 @@ public class NewUser {
 		driver.manage().window().maximize();
 		
 		// Filling details in the name text field
-     	        WebElement nameTextField = driver.findElement(By.name("name"));
+     	WebElement nameTextField = driver.findElement(By.name("name"));
 		Thread.sleep(2000);
 		nameTextField.sendKeys("Rohan Singh");
 		
@@ -37,6 +37,39 @@ public class NewUser {
 		WebElement accountBtn = driver.findElement(By.className("col-sm-push-3"));
 		Thread.sleep(2000);
 		accountBtn.click();
+		
+		Thread.sleep(3000);
+		driver.navigate().to("http://localhost:8080/oms/index.html");
+		
+		// Selecting the email text fields
+		WebElement emailField = driver.findElement(By.tagName("input"));
+		Thread.sleep(1000);
+		emailField.sendKeys("rohansingh@gmail.com");
+				
+		// Selecting the password text fields
+		WebElement passwordField = driver.findElement(By.name("pass"));
+		Thread.sleep(2000);
+		passwordField.sendKeys("654321");
+				
+		// Login button was clicked
+		WebElement loginBtn = driver.findElement(By.cssSelector("input.col-sm-push-3"));
+		Thread.sleep(3000);
+		loginBtn.click();
+		
+		// Selecting the email text fields for the second time
+		WebElement emailField2 = driver.findElement(By.tagName("input"));
+		Thread.sleep(1000);
+		emailField2.sendKeys("rohansingh@gmail.com");
+				
+		// Selecting the password text fields for the second time
+		WebElement passwordField2 = driver.findElement(By.name("pass"));
+		Thread.sleep(2000);
+		passwordField2.sendKeys("654321");
+				
+		// Login button was clicked for the second time
+		WebElement loginBtn2 = driver.findElement(By.cssSelector("input.col-sm-push-3"));
+		Thread.sleep(3000);
+		loginBtn2.click();		
 	
 	}
 
