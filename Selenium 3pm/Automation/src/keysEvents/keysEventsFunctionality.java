17feca5f77402a2837a900e2dd9dc00d2cbdd800
Nitin <nitinsokhal@gmail.com>
2024-12-16 17:24:10 +0530

    Java Selenium Q spiders

---
+++ Selenium 3pm/Automation/src/keysEvents/keysEventsFunctionality.java
@@ -0,0 +1,31 @@
+package keysEvents;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class keysEventsFunctionality {
+
+	public static void main(String[] args) throws AWTException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.get("https://www.facebook.com");
+
+		Robot robotVariable = new Robot();
+		robotVariable.keyPress(KeyEvent.VK_CONTROL);
+		robotVariable.keyPress(KeyEvent.VK_SHIFT);
+		robotVariable.keyPress(KeyEvent.VK_I);
+		
+		robotVariable.keyRelease(KeyEvent.VK_CONTROL);
+		robotVariable.keyRelease(KeyEvent.VK_SHIFT);
+		robotVariable.keyRelease(KeyEvent.VK_I);
+		
+		
+	}
+
+}
